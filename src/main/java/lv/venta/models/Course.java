efc496aea61129783f298eac93cb88f754e0fa5c
Paula <paula.cielava1998@gmail.com>
2023-05-18 10:47:43 +0300

    New: Course klase izveidota

---
+++ src/main/java/lv/venta/models/Course.java
@@ -0,0 +1,48 @@
+package lv.venta.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Table(name = "course_table")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class Course {
+	
+	@Setter(value = AccessLevel.NONE) // -> noņem anotāciju (set funkciju) tieši šim mainīgajam
+	@Column(name = "Idc")
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long idc; //idc - id course
+	
+	@Column(name = "Title")
+	@NotNull
+	@Pattern(regexp = "[A-ZĀŠĒĢŪĪĶĻŅŽ]{1}[a-zēīļķšāžņūģ\\ ]+")
+	@Size(min = 3, max = 30)
+	private String title;
+	
+	@Column(name = "CreditPoints")
+	@Min(value = 1)
+	@Max(value = 20)
+	private int creditPoints;
+	
+	
+	// TODO argumenta konstruktors 
+	
+}
