407b60606c10f5d3aa42be6d205dcd72808a8df5
Paula <paula.cielava1998@gmail.com>
2023-05-18 11:30:51 +0300

    Interface izveidošana visām klasēm

---
+++ src/main/java/lv/venta/repos/IProfessorRepo.java
@@ -0,0 +1,14 @@
+package lv.venta.repos;
+
+import org.springframework.data.repository.CrudRepository;
+
+import lv.venta.models.Professor;
+
+public interface IProfessorRepo extends CrudRepository<Professor, Long>{
+	
+	
+	
+	
+	
+	
+}
