1b51beb38f58498fac506270171b742da84343c0
Paula <paula.cielava1998@gmail.com>
2023-05-18 09:29:30 +0300

    New: new seminar

---
+++ src/main/java/lv/venta/Seminar2ProgrInzApplication.java
@@ -0,0 +1,13 @@
+package lv.venta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Seminar2ProgrInzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Seminar2ProgrInzApplication.class, args);
+	}
+
+}
