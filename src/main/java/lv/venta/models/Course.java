e9532e074b3c6bd591fd5ea3f2274de09fc45288
Paula <paula.cielava1998@gmail.com>
2023-05-19 09:58:33 +0300

    Kļūdas labojums

--- src/main/java/lv/venta/models/Course.java
+++ src/main/java/lv/venta/models/Course.java
@@ -12,7 +12,6 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
