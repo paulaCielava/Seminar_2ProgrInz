635d583b744897a709cc1733dcff96f9a7f3795b
Paula <paula.cielava1998@gmail.com>
2023-05-19 10:32:11 +0300

    Atlasīšanas funkcijas IGradeRepo klasē

--- src/main/java/lv/venta/repos/IGradeRepo.java
+++ src/main/java/lv/venta/repos/IGradeRepo.java
@@ -1,9 +1,19 @@
 package lv.venta.repos;
 
+import java.util.ArrayList;
+
 import org.springframework.data.repository.CrudRepository;
 
 import lv.venta.models.Grade;
 
 public interface IGradeRepo extends CrudRepository<Grade, Long>{
+	//TODO atlasīt visu studentu atzīmes, ja ir zināms studentu id
+	ArrayList<Grade> findByStudentIds (long id);
+	
+	//TODO atlasīt visa kursa atzīmes, ja ir zināms kursa nosaukums
+	ArrayList<Grade> findByCourseTitle(String title);
+
+
+
 
 }
